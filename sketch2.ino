8218d6521bf7cd9884472128ad1b088f943faa09
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-06-29 07:20:14 +0530

    Driving a Seven Segment Display with an arduino

---
+++ sketch2.ino
@@ -0,0 +1,63 @@
+#define A 2
+#define B 3
+#define C 4
+#define D 5
+#define E 6
+#define F 7
+#define G 8
+
+void setup(){
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  delay(1000);
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  delay(1000);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(G, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(D, HIGH);
+  delay(1000);
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(D, LOW);
+
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(G, HIGH);
+  delay(1000);
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(G, LOW);
+}
